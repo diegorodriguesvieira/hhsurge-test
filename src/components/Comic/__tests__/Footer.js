5c64ab5b52ce0b70e998bd3653aa6eefd6dc5a22
Diego <diegorodrigues92@gmail.com>
2020-02-04 01:28:15 -0300

    Improves comic's footer test

--- src/components/Comic/__tests__/Footer.js
+++ src/components/Comic/__tests__/Footer.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import React from 'react';
 
 import Footer from '../Footer';
@@ -12,5 +12,25 @@ describe('Footer', () => {
   test('Should not render <Footer />', async () => {
     const { queryByTestId } = render(<Footer url="" />);
     expect(queryByTestId('comic-more-details')).toBeNull();
+  });
+
+  test('Should not render <Footer />', async () => {
+    const { queryByTestId } = render(<Footer url="" />);
+    expect(queryByTestId('comic-more-details')).toBeNull();
+  });
+
+  test('Should call window.open once', async () => {
+    jest.spyOn(window, 'open').mockImplementationOnce(() => {
+      return true;
+    });
+
+    const mockUrl = '/some/url';
+    const { getByTestId } = render(<Footer url={mockUrl} />);
+
+    fireEvent.click(getByTestId('comic-more-details'));
+
+    expect(window.open).toBeCalledTimes(1);
+    expect(window.open).toBeCalledWith(mockUrl, '_blank');
+    expect(window.open).toBeTruthy();
   });
 });
