ae8cce3fea81e50ee574aaaccee11e555c58d028
Diego <diegorodrigues92@gmail.com>
2020-02-04 02:44:37 -0300

    Fix service axios

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -1,3 +1,4 @@
 export default {
   get: jest.fn().mockResolvedValue({ data: {} }),
+  create: jest.fn().mockReturnValue({ interceptors: { request: { use: jest.fn() } } }),
 };
--- src/services/axios.js
+++ src/services/axios.js
@@ -1,15 +1,19 @@
 import axios from 'axios';
 
-export const marvelAPIUrl = 'https://gateway.marvel.com:443/v1/public';
+export const marvelAPIUrl = 'https://gateway.marvel.com/v1/public';
 export const apiKey = 'b4f55cae9e47788b52ff355bffbc1fe1';
 
 const marvelAPI = axios.create({
   baseURL: marvelAPIUrl,
-  params: {
-    apiKey,
-  },
   timeout: 10000,
   responseType: 'json',
+});
+
+marvelAPI.interceptors.request.use(config => {
+  const newConfig = config;
+  newConfig.params.apikey = apiKey;
+
+  return newConfig;
 });
 
 export default marvelAPI;
